d3635fc04a1747dcd3ba91f40511ddf8503d37cb
Masoud Abdaal <m_abdaal@outlook.com>
2024-07-28 06:29:05 +0330

    Added http api

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func rootApi(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "welcome to back-end\n")
+
+	body, _ := io.ReadAll(r.Body)
+
+	fmt.Println(r.Method, r.URL, r.Proto, r.Host,
+		"<--"+r.RemoteAddr, "( "+string(body)+" )")
+
+	for name, values := range r.Header {
+		if strings.HasPrefix(name, "X-") {
+			fmt.Print(name, values, "\n")
+		}
+	}
+
+}
+
+func handleReq() {
+	http.HandleFunc("/", rootApi)
+	log.Fatal(http.ListenAndServe(":1080", nil))
+}
 
 func main() {
-	fmt.Printf("Hello from smuggling\n")
+	handleReq()
 }
